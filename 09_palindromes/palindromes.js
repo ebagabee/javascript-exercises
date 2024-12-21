6340a17e8bf0c988f1c78530c36b9486e9fbb764
ebagabee <gabrielhas.tech@gmail.com>
2024-12-21 14:00:03 -0300

    palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const onlyAlphaNumeric = string.toLowerCase().match(/[a-z0-9]/g);
 
+  if (!onlyAlphaNumeric) return false;
+
+  const cleanedString = onlyAlphaNumeric.join("");
+
+  const reversedString = [...cleanedString].reverse().join("");
+
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
