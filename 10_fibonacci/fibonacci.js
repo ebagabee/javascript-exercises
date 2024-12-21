8bb0ec364475f347e63c9fc6b8f52fcbc1a888df
ebagabee <gabrielhas.tech@gmail.com>
2024-12-21 14:00:33 -0300

    fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  if (isNaN(number) || number < 0) {
+    return "OOPS";
+  }
 
+  const fibonacciArray = [0, 1];
+
+  for (let i = 0; fibonacciArray.length <= number; i++) {
+    fibonacciArray.push(fibonacciArray[i] + fibonacciArray[i + 1]);
+  }
+
+  return fibonacciArray[number];
 };
+
+fibonacci(8);
 
 // Do not edit below this line
 module.exports = fibonacci;
