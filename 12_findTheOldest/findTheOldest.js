1d25f47697f5df182f0515653b9d1a7a414b00d8
ebagabee <gabrielhas.tech@gmail.com>
2024-12-21 14:30:39 -0300

    findTheOldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const verifyObjectHasDeathYear = (people) => {
+  if (!people.hasOwnProperty("yearOfDeath")) {
+    people.yearToday = new Date().getFullYear();
+    return false;
+  }
 
+  return true;
+};
+
+const findTheOldest = function (array) {
+  let theBiggestDifference = 0;
+  let oldestPerson = null;
+
+  for (let people of array) {
+    let currentDifference = 0;
+
+    if (verifyObjectHasDeathYear(people)) {
+      currentDifference += people.yearOfDeath - people.yearOfBirth;
+    } else {
+      currentDifference += people.yearToday - people.yearOfBirth;
+    }
+
+    if (currentDifference > theBiggestDifference) {
+      theBiggestDifference = currentDifference;
+      oldestPerson = people;
+    }
+  }
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
